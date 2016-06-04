325b6ddb9e1838fc2655364316f3bd9b6fda755d
handrin <handrin@uwo.ca>
2016-06-04 01:11:19 -0400

    Validate canadian postal code

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,14 +2,15 @@
   var app = angular.module('exampleWebpage', []);
   app.controller('InformationController', function(){
     this.information = {};
+    this.validPostalCode = false;
 
     this.addInformation = function() {
       this.information = {};
+      this.isCanada();
     };
 
     this.isCanada = function() {
       var isCanada = false;
-      console.log(this.information.addressDetails.country.toLowerCase());
       if (this.information.addressDetails.country.toLowerCase() === "canada") {
         isCanada = true;
       } else {
@@ -17,6 +18,23 @@
       }
       return isCanada;
     }
-  });
 
+    this.isValidCanadianPostalCode = function() {
+      var isValid = false;
+      var postalCode = this.information.addressDetails.postalCode;
+      var regex = /^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$/;
+      var match = regex.exec(postalCode);
+      if (match){
+          if ( (postalCode.indexOf("-") !== -1 || postalCode.indexOf(" ") !== -1 ) && postalCode.length === 7 ) {
+              isValid = true;
+          } else if ( (postalCode.indexOf("-") == -1 || postalCode.indexOf(" ") == -1 ) && postalCode.length === 6 ) {
+              isValid = true;
+          }
+      } else {
+              isValid = false;
+      }
+      this.validPostalCode = isValid;
+      return isValid;
+    }
+  });
 })();
