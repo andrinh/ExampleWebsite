c88352fa01a270d255f1869f1f960a6af0df625c
handrin <handrin@uwo.ca>
2016-06-03 23:55:17 -0400

    Validate the form

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,8 +1,19 @@
 (function() {
-  var app = angular.module('gemStore', []);
-
+  var app = angular.module('exampleWebpage', []);
   app.controller('InformationController', function(){
     this.information = {};
+
+    this.addInformation = function() {
+      this.information = {};
+    };
+
+    this.isCanada = function() {
+      boolean isCanada = false;
+      if (this.information.addressDetails.province.countrytoLowerCase() === "canada") {
+        isCanada = true;
+      }
+      return isCanada;
+    }
   });
 
 })();
