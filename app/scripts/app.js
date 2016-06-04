51c443b44e3526f0f60d3d002ca6600036a3cd85
handrin <handrin@uwo.ca>
2016-06-03 23:04:32 -0400

    Properly align form module

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,14 +2,7 @@
   var app = angular.module('gemStore', []);
 
   app.controller('InformationController', function(){
-    this.information = info;
+    this.information = {};
   });
 
-  var info = [{
-      name: "Hunter Andrin",
-      email: "andrinhunter@gmail.com",
-      address: "London Ontario",
-      postal_code: "N6A 2K1",
-      phone_number: "226-236-7427"
-    }]
 })();
